91344035dac31d15fa04d63b848d9a63e8559213
sumit07sinha <sumit07sinha@gmail.com>
2020-05-10 10:21:04 +0530

    service worker added

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,7 +2,7 @@ const path = require("path");
 const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-
+const WorkboxPlugin = require("workbox-webpack-plugin");
 module.exports = {
   entry: "./src/client/index.js",
   mode: "development",
@@ -38,6 +38,31 @@ module.exports = {
       // Automatically remove all unused webpack assets on rebuild
       cleanStaleWebpackAssets: true,
       protectWebpackAssets: false,
+    }),
+    new WorkboxPlugin.GenerateSW({
+      // Do not precache images
+      exclude: [/\.(?:png|jpg|jpeg|svg)$/],
+
+      // Define runtime caching rules.
+      runtimeCaching: [
+        {
+          // Match any request that ends with .png, .jpg, .jpeg or .svg.
+          urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+
+          // Apply a cache-first strategy.
+          handler: "CacheFirst",
+
+          options: {
+            // Use a custom cache name.
+            cacheName: "images",
+
+            // Only cache 10 images.
+            expiration: {
+              maxEntries: 10,
+            },
+          },
+        },
+      ],
     }),
   ],
 };
