d0134e30b8d81d9d81ef3e0bd0b1c014c8a5593b
sumit07sinha <sumit07sinha@gmail.com>
2020-05-10 13:47:19 +0530

    localserver reset

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -7,7 +7,7 @@ function handleSubmit(event) {
     console.log("::: FORM INPUT VALID :::");
 
     console.log("BUILDING REQUEST");
-    fetch("http://localhost:3030/article", {
+    fetch("http://localhost:3000/article", {
       method: "POST",
       mode: "cors",
       headers: {
